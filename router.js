c2e8b1992c50fa992571f7d3f1bff1902d9ce2b4
imriven <christinamharris83@gmail.com>
2022-04-19 13:31:56 -0700

    logic for addition route

--- router.js
+++ router.js
@@ -8,16 +8,11 @@ router.get("add/:num1/:num2", (req, res) => {
     let num1 = req.params.num1
     let num2 = req.params.num2
     let result = num1 + num2
-    .then((result) => {
       if (!result) {
         res.status(404).json({ error: "Can't add" });
       } else {
         res.status(200).json(result);
       }
-    })
-    .catch((err) =>
-      res.status(500).json({ error: `Error connecting to database, ${err}` })
-    );
 });
 
 module.exports = router;
