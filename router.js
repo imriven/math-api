b00a3f8aaf3e433159e9e0dfc77cdc8812b49b96
imriven <christinamharris83@gmail.com>
2022-04-19 13:24:13 -0700

    logic for addition route

--- router.js
+++ router.js
@@ -4,14 +4,15 @@ const bcrypt = require("bcryptjs");
 
 
 
-router.get("add/{num1}/{num2}", (req, res) => {
+router.get("add/:num1/:num2", (req, res) => {
+    let num1 = req.params.num1
+    let num2 = req.params.num2
     let result = num1 + num2
-  req.params
     .then((result) => {
       if (!result) {
         res.status(404).json({ error: "Can't add" });
       } else {
-        res.status(200).json(result);s
+        res.status(200).json(result);
       }
     })
     .catch((err) =>
